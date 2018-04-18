b34de921596322df108a0eb526ec7282525f8f57
Uthaeus <romanlavery@gmail.com>
2018-04-18 13:40:48 -0600

    Calculated and added conditions and logic to calculate countdown correctly

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -18,6 +18,23 @@ class Clock extends Component {
     var bday = new Date(birthday);
     let today = new Date();
 
+    const currentMonth = today.getMonth();
+    const birthdayMonth = bday.getMonth();
+
+    if (birthdayMonth > currentMonth) {
+      bday.setFullYear(today.getFullYear());
+    } else if (birthdayMonth < currentMonth) {
+      bday.setFullYear(today.getFullYear() + 1);
+    } else if (birthdayMonth == currentMonth) {
+      const birthDay = bday.getDate();
+      const currentDay = today.getDate();
+      if (birthDay > currentDay) {
+        bday.setFullYear(today.getFullYear());
+      } else if (birthDay < currentDay) {
+        bday.setFullYear(today.getFullYear() + 1);
+      }
+    }
+
     var distance = bday.getTime() - today.getTime();
 
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
@@ -37,6 +54,9 @@ class Clock extends Component {
   getAge = function() {
     var bday = new Date(this.birthday);
     let today = new Date();
+
+    
+
     var distance = bday.getTime() - today.getTime();
     var daysOld = Math.floor(distance / (1000 * 60 * 60 * 24));
     var yearsOld = Number((daysOld / 365).toFixed(0));
